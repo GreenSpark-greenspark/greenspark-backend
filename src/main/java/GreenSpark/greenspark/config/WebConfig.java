b76db24d4dd8a949d3bc462057234483fdbbb128
jhj2075 <flg989@naver.com>
2024-11-14 14:30:06 +0900

    fix: cors 에러 재재재수정

--- src/main/java/GreenSpark/greenspark/config/WebConfig.java
+++ src/main/java/GreenSpark/greenspark/config/WebConfig.java
@@ -9,14 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("*")
-                .allowedMethods(
-                HttpMethod.GET.name(),
-                HttpMethod.HEAD.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.PATCH.name(),
-                HttpMethod.DELETE.name());
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
     }
 }
