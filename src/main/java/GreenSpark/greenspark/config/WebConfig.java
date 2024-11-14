cdc6a19fcf5757f048194db0ce6145101c9619a9
jhj2075 <flg989@naver.com>
2024-11-14 22:37:00 +0900

    fix: nginx 연결을 위한 설정 수정

--- src/main/java/GreenSpark/greenspark/config/WebConfig.java
+++ src/main/java/GreenSpark/greenspark/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("https://green-spark.vercel.app", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
