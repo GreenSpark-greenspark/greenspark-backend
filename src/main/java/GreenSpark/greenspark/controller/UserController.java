15b0be418dcd2ff39996d00631ddf5c2536dd78a
Seonjeongg <sj030205@naver.com>
2024-11-23 11:14:58 +0900

    feat: UserController RequestMapping 수정

--- src/main/java/GreenSpark/greenspark/controller/UserController.java
+++ src/main/java/GreenSpark/greenspark/controller/UserController.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
